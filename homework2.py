953db934771780dfdbf8a441f6ab3d955a636fc2
MoskvichevGeorge <moskvicevg@gmail.com>
2024-05-15 21:38:31 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,11 @@
+name = 'Георгий'
+print("Name:" + name)
+age = 20
+print("Age:" + (str(age)))
+age = 20
+name = age + 1
+print('New Age:' + (str(name)))
+status = True
+print("Is student:" + (str(status)))
+
+
