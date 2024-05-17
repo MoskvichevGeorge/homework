c1cd513901e77a0e2979033ca250c66a48bb041b
MoskvichevGeorge <moskvicevg@gmail.com>
2024-05-17 22:06:19 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list =  ['apple', 'tamato', 'patato', 'cucumber']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2 : 5])
+my_list[3] = 'grape'
+print(my_list)
+
+my_dict = {'tamato' : "помидор" , "apple" : "яблоко" , "patato" : "картофель" , "cucumber" : "огурец"}
+print(my_dict)
+b = my_dict.pop("tamato")
+print(b)
+my_dict["orange"] = "апельсин"
+print(my_dict)
