daf8db31745afb4a3426320349a20e27bc61066c
MoskvichevGeorge <moskvicevg@gmail.com>
2024-05-19 20:49:16 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -1,6 +1,7 @@
 immutable_var = (1 , 3 , "data" , False)
 print(immutable_var)
 print(type(immutable_var))
+immutable_var[0] = 10 #у типа tuple нет методов для добавления и удаления элементов.
 
 mutable_list = [1 , 'base' , 'apple' , 13]
 mutable_list[0] = 23
