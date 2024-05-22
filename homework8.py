105f8900014d308527ae94e12a51b762b827585d
MoskvichevGeorge <moskvicevg@gmail.com>
2024-05-22 22:24:07 +0300

    Add files via upload

--- homework8.py
+++ homework8.py
@@ -1,7 +1,7 @@
 list_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 for i in list_:
     print('Я езжу на автомабиле марки ' + i)
-counter = 0
+cars_count = 0
 for k in list_:
-    counter += 10
-    print(f"counter: {counter}")
+    cars_count += 10
+    print(f"cars_count: {cars_count}")
