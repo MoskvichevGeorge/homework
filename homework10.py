079fb65658452ab04a2b47873a5901185150bbfb
MoskvichevGeorge <moskvicevg@gmail.com>
2024-05-28 21:17:23 +0300

    Add files via upload

---
+++ homework10.py
@@ -0,0 +1,12 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    for word in other_words:
+        if root_word in word or word in root_word:
+            same_words.append(word)
+    return same_words
+
+
+root_word = 'rich'
+other_words = ['richiest', 'orichalcum', 'cheers', 'richies']
+result = single_root_words(root_word, *other_words)
+print(result)
