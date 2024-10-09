f3471239e6613bd0e0088d97e2b9430b24203e20
MoskvichevGeorge <moskvicevg@gmail.com>
2024-10-09 17:38:27 +0300

    Update module 8.1.py

--- module 8.1.py
+++ module 8.1.py
@@ -1,13 +1,9 @@
 def add_everything_up(a, b):
-    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
-
+    try:
         return a + b
-    elif isinstance(a, str) and isinstance(b, str):
-
-        return a + b
-    else:
-
+    except TypeError:
         return str(a) + str(b)
+    
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
