7f61bbcaf30d3867b9c671e91ed263fcdf886687
MoskvichevGeorge <moskvicevg@gmail.com>
2024-12-24 17:48:07 +0300

    Add files via upload

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,19 +1,21 @@
 def calculate_structure_sum(data):
     total_sum = 0
 
-    if isinstance(data, (list, tuple)):
+    #
+    if isinstance(data, dict):
+        for key, value in data.items():
+            total_sum += calculate_structure_sum(key)
+            total_sum += calculate_structure_sum(value)
+    elif isinstance(data, (list, tuple, set)):
         for item in data:
             total_sum += calculate_structure_sum(item)
-    elif isinstance(data, dict):
-        for key, value in data.items():
-            total_sum += calculate_structure_sum(key)  
-            total_sum += calculate_structure_sum(value)
     elif isinstance(data, str):
         total_sum += len(data)
     elif isinstance(data, (int, float)):
         total_sum += data
 
     return total_sum
+
 
 data_structure = [
     [1, 2, 3],
